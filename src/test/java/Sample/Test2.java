e77a74c1f9047a538a82ad1a04e6c3004df55e3e
Renuka Jadhav <renuka.wadekar1990@gmail.com>
2025-05-15 16:52:51 +0530

    Github working check commit

---
+++ src/test/java/Sample/Test2.java
@@ -0,0 +1,10 @@
+package Sample;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Test github not working");
+	}
+
+}
